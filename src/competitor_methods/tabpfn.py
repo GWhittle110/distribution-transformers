Fixed KL div estimation and correct batching in TabPFN

src/competitor_methods/tabpfn.py
@@ -1,11 +1,13 @@
 import torch
+import tqdm
+from competitor_methods.pfns import RiemannDistribution
 from tabpfn import TabPFNRegressor  
 
 
 def construct_tabpfn_inputs(
     phi, x, z
 ):
-    batch_size = x.shape[0]
+    batch_size = x.shape[0] if len(x.shape) > 0 else 1
 
     phi_keys_sorted = sorted([key for key in phi.keys()])
     obs_keys_sorted = sorted([key for key in z.keys()])
@@ -22,9 +24,14 @@ def construct_tabpfn_inputs(
 def test_tabpfn(
     phi_dict_test, x_test, z_test,
     complete_distribution,
-    tabpfn_trainsize
+    tabpfn_trainsize,
+    cap_logits=-100.0
     ):
-        
+
+    # PFN-type models can only handle univariate outputs, hence event shape = ()
+    batch_size = x_test.shape
+    device = z_test[list(z_test.keys())[0]].device
+    
     phi_train, x_train, z_train = complete_distribution.sample((tabpfn_trainsize,))
     phi_prior_dict = complete_distribution.meta_prior.decode_sample(phi_train)
 
@@ -33,60 +40,20 @@ def test_tabpfn(
 
     regressor = TabPFNRegressor()
 
-    regressor.fit(X_train, Y_train)
+    regressor.fit(X_train.cpu(), Y_train.cpu())
+
+    regressor_prediction = regressor.predict(X_test.cpu(), output_type="full")
+    logits = regressor_prediction['logits']
+    criterion = regressor_prediction['criterion']
     
-    regressor_prediction = regressor.predict(X_test, output_type="full")
-    
-    output_distribution = TabPFNOutputDistribution(regressor_prediction)
-    nll = -output_distribution.log_prob(Y_test)
+    logits = torch.clamp(logits, min=cap_logits)
+     
+    output_distribution = RiemannDistribution(
+        probs=logits.softmax(dim=-1).reshape(batch_size + (-1,)).to(device), 
+        borders=criterion.borders.to(device),
+    )
+
+    nll = -output_distribution.log_prob(Y_test.swapdims(0, -1).to(device))
 
     return output_distribution, nll, regressor
 
-
-class TabPFNOutputDistribution(torch.distributions.Distribution):
-    arg_constraints = {}
-    support = torch.distributions.constraints.real
-    has_rsample = False
-
-    def __init__(self, regressor_prediction: dict):
-        """
-        Custom distribution class for TabPFN outputs.
-
-        """
-        batch_shape = regressor_prediction['logits'].shape[:-1]
-        if len(batch_shape) == 1:
-            batch_shape = torch.Size(())
-        super().__init__(batch_shape=batch_shape, event_shape=torch.Size(()))
-        self.regressor_prediction = regressor_prediction
-    
-    def log_prob(self, value: torch.Tensor) -> torch.Tensor:
-        """
-        Compute log probability of value under the TabPFN output distribution.
-
-        Args:
-            value: Value at which to evaluate log probability of shape (...,).
-
-        Returns:
-            Log probability of shape (...,).
-        """
-        criterion = self.regressor_prediction['criterion'].cpu()
-        logits = self.regressor_prediction['logits'].cpu()
-        batch_size = value.shape[0]
-
-        value = value.reshape(batch_size, -1)
-        logits = logits.expand(batch_size, -1)
-        return -criterion.forward(logits, value.cpu()).reshape(batch_size)
-
-    def sample(self, sample_shape: torch.Size = torch.Size()) -> torch.Tensor:
-        """
-        Sample from the TabPFN output distribution.
-
-        Args:
-            sample_shape: Shape of samples to draw.
-
-        Returns:
-            Samples from the TabPFN output distribution.
-        """
-        criterion = self.regressor_prediction['criterion'].cpu()
-        logits = self.regressor_prediction['logits'].cpu()
-        return criterion.sample(logits, sample_shape)